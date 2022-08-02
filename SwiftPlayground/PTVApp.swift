78761adf78b7aeaa7b9ec2275de7be79d625c43f
Alex Stonehouse <alex.stonehouse@tigerspike.com>
2022-08-02 19:32:38 +1000

    Moved from AppDelegate to @main

---
+++ SwiftPlayground/PTVApp.swift
@@ -0,0 +1,18 @@
+//
+//  PTVApp.swift
+//  SwiftPlayground
+//
+//  Created by Alex Stonehouse on 2/8/2022.
+//  Copyright © 2022 alex. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct PTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
